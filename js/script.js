b5b4a0e05e3f4456209a4e881744892c2e608473
Angelo31294 <luongoangelo1@gmail.com>
2022-11-02 16:25:05 +0100

    Agg. evento click "playgame"

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@
 // seleziono il contenitore dei numeri
 const boardContainer = document.querySelector(".board");
 const playGame = document.querySelector(`.btn-play`);
-
-playGame.addEventListener("cilck", function(){
     
     boardContainer.innerHTML = "";
 
-    for( let i = 1; i <= 100; i++ ){
-        const boardCell = document.createElement("div");
-        boardCell.innerHTML = i;
-        boardCell.classList.add("board-number");
-        // aggiungo l'evento click
-        boardCell.addEventListener("click", function(){
-            boardCell.classList.add("select");
-            console.log(this);
-        });
-        boardContainer.append(boardCell);
-    }
-
-    boardCell.classList.add("show");
+playGame.addEventListener("click", function () {
+    boardContainer.classList.add("show")
 })
+
+for( let i = 1; i <= 100; i++ ){
+    const boardCell = document.createElement("div");
+    boardCell.innerHTML = i;
+    boardCell.classList.add("board-number");
+    // aggiungo l'evento click
+    boardCell.addEventListener("click", function(){
+        boardCell.classList.add("select");
+        console.log(this);
+    });
+    boardContainer.append(boardCell);
+}
+
 
